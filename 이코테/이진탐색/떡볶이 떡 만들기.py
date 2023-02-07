6bfe2667a30ad9bf458b95797f948d8773536b86
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-02-07 18:35:24 +0900

    [Review] 이코테 이진탐색 복습 및 코드 수정

--- 이코테/이진탐색/떡볶이 떡 만들기.py
+++ 이코테/이진탐색/떡볶이 떡 만들기.py
@@ -1,31 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-def totalSum(heights, height):
+n, m = map(int, input().split())
+heights = list(map(int, input().split()))
+heights.sort()
+
+def getSum(h):
     sum = 0
-    for h in heights:
-        if h > height:
-            sum += (h-height)
+    for i in heights:
+        if i > h:
+           sum += (i - h)
     return sum
 
-def binarySearchHeight(heights, start, end, overSum):
-    result = 0
+def paramatric_search(start, end, sum):
+    temp = 0
     while start <= end:
-        midHeight = (start+end)//2
-        sum = totalSum(heights, midHeight)
-        if sum == overSum:
-            return midHeight
-        elif sum < overSum:
-            end = midHeight-1
+        mid = (start + end) // 2
+        currentSum = getSum(mid)
+        if currentSum == sum:
+            return mid
+        elif currentSum < sum:
+            end = mid - 1
         else:
-            result = midHeight
-            start = midHeight+1
-    return result
+            temp = mid
+            start = mid + 1
+    return temp
 
-# 자투리 합산: M 이상이 되기 위한 최대 H 구하기
-N, M = map(int, input().split())
-heights = list(map(int, input().split()))
-
-# H 최소높이, 최대높이 값을 구한 후 binarySearch 를 통해 height 값을 찾는다
-finalHeight = binarySearchHeight(heights, min(heights), max(heights), M)
-print(finalHeight)
+print(paramatric_search(min(heights), max(heights), m))
--- 이코테/이진탐색/부품 찾기.py
+++ 이코테/이진탐색/부품 찾기.py
@@ -1,30 +1,65 @@
+# import sys
+# input = sys.stdin.readline
+
+# def binarySearch(array, start, end, target):
+#     while start <= end:
+#         mid = (start+end)//2
+#         if array[mid] == target:
+#             return mid
+#         elif target < array[mid]:
+#             end = mid-1
+#         else:
+#             start = mid+1
+#     return -1
+
+# def find(items, count, target):
+#     return binarySearch(items, 0, count-1, target) != -1
+
+# N = int(input())
+# items = list(map(int, input().split()))
+# items.sort()
+
+# M = int(input())
+# questions = list(map(int, input().split()))
+
+# for i in range(M):
+#     if find(items, N, questions[i]):
+#         print("yes", end=" ")
+#     else:
+#         print("no", end=" ")
+# print()
+
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# items = list(map(int, input().split()))
+# m = int(input())
+# orders = list(map(int, input().split()))
+
+# items.sort()
+
+# def binary_search(target, start, end):
+#     while start <= end:
+#         mid = (start + end) // 2
+#         if target == items[mid]:
+#             return True
+#         elif target < items[mid]:
+#             end = mid - 1
+#         else:
+#             start = mid + 1
+#     return False
+
+# for i in orders:
+#     print("yes" if binary_search(i, 0, len(items)) else "no", end=" ")
+
 import sys
 input = sys.stdin.readline
 
-def binarySearch(array, start, end, target):
-    while start <= end:
-        mid = (start+end)//2
-        if array[mid] == target:
-            return mid
-        elif target < array[mid]:
-            end = mid-1
-        else:
-            start = mid+1
-    return -1
+n = int(input())
+items = set(map(int, input().split()))
+m = int(input())
+orders = list(map(int, input().split()))
 
-def find(items, count, target):
-    return binarySearch(items, 0, count-1, target) != -1
-
-N = int(input())
-items = list(map(int, input().split()))
-items.sort()
-
-M = int(input())
-questions = list(map(int, input().split()))
-
-for i in range(M):
-    if find(items, N, questions[i]):
-        print("yes", end=" ")
-    else:
-        print("no", end=" ")
-print()
+for i in orders:
+    print("yes" if i in items else "no", end=" ")
