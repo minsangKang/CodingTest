5463e1e7a4b0f5c4dc4913d19f8ffeec3e09d127
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-02-07 13:00:57 +0900

    [Review] 이코테 DFS, BFS 복습 및 코드 수정

--- 이코테/DFS:BFS/Python/미로 탈출.py
+++ 이코테/DFS:BFS/Python/미로 탈출.py
@@ -1,36 +1,26 @@
 from collections import deque
-# 1: 움직일 수 있는 칸
-# 최소로 움직일 수 있는 거리 -> BFS로 한칸씩 증가하며 확인
-move = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-
-def bfs(n, m, graph):
-    queue = deque()
-    queue.append((0, 0))
-
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nextX = x + move[i][0]
-            nextY = y + move[i][1]
-
-            if nextX < 0 or nextX >= n or nextY < 0 or nextY >= m:
-                continue
-            if graph[nextX][nextY] == 1:
-                graph[nextX][nextY] = graph[x][y]+1
-                queue.append((nextX, nextY))
-    
-    return graph[n-1][m-1]
-
-
-def solution(n, m, graph):
-    return bfs(n, m, graph)
+moves = [(-1, 0), (1, 0), (0, -1), (0, 1)] #상, 하, 좌, 우
 
 n, m = map(int, input().split())
-graph = []
-for row in range(n):
-    line = [int(x) for x in list(input())]
-    graph.append(line)
+graph = [list(map(int, input())) for _ in range(n)]
 
-answer = solution(n, m, graph)
-print(answer)
+def bfs():
+    que = deque()
+    que.append((0, 0))
+    
+    while que:
+        x, y = que.popleft()
+        for move in moves:
+            nx = x + move[0]
+            ny = y + move[1]
+            
+            if nx < 0 or nx >= n:
+                continue
+            if ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y]+1
+                que.append((nx, ny))
+
+bfs()
+print(graph[n-1][m-1])
--- 이코테/DFS:BFS/Python/음료수 얼려 먹기.py
+++ 이코테/DFS:BFS/Python/음료수 얼려 먹기.py
@@ -1,32 +1,27 @@
-def dfs(row, column, n, m, visited, graph):
-    visited[row][column] = True
-    if row-1 >= 0 and visited[row-1][column] == False and graph[row-1][column] == 0:
-        dfs(row-1, column, n, m, visited, graph)
-    if column-1 >= 0 and visited[row][column-1] == False and graph[row][column-1] == 0:
-        dfs(row, column-1, n, m, visited, graph)
-    if row+1 <= n-1 and visited[row+1][column] == False and graph[row+1][column] == 0:
-        dfs(row+1, column, n, m, visited, graph)
-    if column+1 <= m-1 and visited[row][column+1] == False and graph[row][column+1] == 0:
-        dfs(row, column+1, n, m, visited, graph)
-
-
-def solution(n, m, graph):
-    visited = [[False] * m for i in range(n)]
-    count = 0
-
-    for row in range(n):
-        for column in range(m):
-            if graph[row][column] == 0 and visited[row][column] != True:
-                count += 1
-                dfs(row, column, n, m, visited, graph)
-    
-    return count
+moves = [(-1, 0), (1, 0), (0, -1), (0, 1)] #상, 하, 좌, 우
 
 n, m = map(int, input().split())
-map = []
-for row in range(n):
-    line = [int(x) for x in list(input())]
-    map.append(line)
+graph = [list(map(int, input())) for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
 
-answer = solution(n, m, map)
-print(answer)
+def dfs(x, y):
+    visited[x][y] = True
+    # (x, y) 기준 인접한 상, 하, 좌, 우 탐색
+    for move in moves:
+        nx = x + move[0]
+        ny = y + move[1]
+        if nx < 0 or nx >= n:
+            continue
+        if ny < 0 or ny >= m:
+            continue
+        if visited[nx][ny] == False and graph[nx][ny] == 0:
+            dfs(nx, ny)
+
+count = 0
+for x in range(n):
+    for y in range(m):
+        if visited[x][y] == False and graph[x][y] == 0:
+            dfs(x, y)
+            count += 1
+            
+print(count)
