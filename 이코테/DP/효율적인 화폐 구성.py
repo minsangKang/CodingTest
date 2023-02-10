767744cabb2e9129415a6be4d68f5a6ecec93dce
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-02-10 13:45:59 +0900

    [Review] 이코테 DP 복습 및 코드 수정

--- 이코테/DP/효율적인 화폐 구성.py
+++ 이코테/DP/효율적인 화폐 구성.py
@@ -1,18 +1,14 @@
 INF = 10001
-N, M = map(int, input().split())
-moneys = []
-for i in range(N):
-    moneys.append(int(input()))
+n, m = map(int, input().split())
+moneys = [int(input()) for _ in range(n)]
 moneys.sort()
 
-dp = [INF]*(M+1)
+dp = [INF]*(m+1)
 dp[0] = 0
 
-# 작은 화폐부터 시작하여 채운다
-for m in moneys:
-    count = 1
-    while(m*count <= M):
-        dp[m*count] = min(dp[m*count] ,dp[m*(count-1)] + 1)
-        count += 1
-
-print(dp[M] if dp[M] != INF else -1)
+for money in moneys:
+    for i in range(money, m+1):
+        if dp[i-money] != INF:
+            dp[i] = min(dp[i], dp[i-money]+1)
+        
+print(dp[m] if dp[m] != INF else -1)
