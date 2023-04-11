fa01fc8abe542939757d60d22d4eabcf03eea2cb
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-04-12 00:02:09 +0900

    [Refactor] 구현: 뱀 새로운 방식으로 구현

--- 이코테/구현/기출문제/뱀.py
+++ 이코테/구현/기출문제/뱀.py
@@ -1,67 +1,66 @@
-# 게임이 몇 초에 끝나는지 출력
 from collections import deque
 move = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-headDirection = 0
-tailDirection = 0
 
 n = int(input())
 k = int(input())
 
-graph = [[0]*n for _ in range(n)]
-apples = []
-for _ in range(k):
-    x, y = map(int, input().split())
-    graph[x-1][y-1] = 2
+graph = [[0]*(n+1) for _ in range(n+1)]
 
+# 사과 정보 입력
+for _ in range(k):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+
+# 회전 정보 입력
+rotations = []
 l = int(input())
-commands = deque()
 for _ in range(l):
     x, c = input().split()
-    commands.append((int(x), c))
+    rotations.append((int(x), c))
 
+# 시뮬레이션 설정
+headX, headY = 1, 1
+graph[headX][headY] = 2
+direction = 0
 time = 0
-headX, headY = 0, 0
-tailX, tailY = 0, 0
-graph[headX][headY] = 1
-commandIndex = 0
-tailCommands = deque()
-tailCommandIndex = -1
-term = 1
+index = 0 # rotation 인텍스
+q = deque()
+q.append((headX, headY)) # 뱀이 차지하고 있는 위치 정보
 
 while True:
+    nextX = headX + move[direction][0]
+    nextY = headY + move[direction][1]
     time += 1
-    nextX, nextY = headX + move[headDirection][0], headY + move[headDirection][1]
-    # 맵을 벗어난 경우
-    if nextX < 0 or nextX >= n or nextY < 0 or nextY >= n:
+    
+    # 이동불가 or 몸통과 만난 경우 종료
+    if nextX < 1 or nextX > n:
         break
-    # 몸과 만난 경우
+    if nextY < 1 or nextY > n:
+        break
+    if graph[nextX][nextY] == 2:
+        break
+
+    # 사과가 위치한 경우 머리이동
     if graph[nextX][nextY] == 1:
-        break
-    # 머리 이동 반영
-    headX, headY = nextX, nextY
-    # 사과가 없는 곳으로 이동한 경우 꼬리 이동
-    if graph[headX][headY] != 2:
+        headX, headY = nextX, nextY
+        graph[headX][headY] = 2
+        q.append((headX, headY))
+
+    # 사과가 위치하지 않은 경우 머리이동 후 꼬리위치 제거
+    if graph[nextX][nextY] == 0:
+        headX, headY = nextX, nextY
+        graph[headX][headY] = 2
+        q.append((headX, headY))
+
+        tailX, tailY = q.popleft()
         graph[tailX][tailY] = 0
-        tailX, tailY = tailX + move[tailDirection][0], tailY + move[tailDirection][1]
-    else:
-        term += 1
-    graph[headX][headY] = 1
-    # 회전여부 확인
-    if len(commands) != 0 and time == commands[0][0]:
-        if commands[0][1] == 'D':
-            headDirection += 1
+    
+    # 회전시간인지 여부 확인
+    if index < l and rotations[index][0] == time:
+        if rotations[index][1] == 'L':
+            direction = (direction - 1) % 4
         else:
-            headDirection -= 1
-        headDirection %= 4
-        tailCommands.append((time+term, commands[0][1]))
-        commands.popleft()
-    # 꼬리 회전여부 확인
-    if len(tailCommands) != 0 and time == tailCommands[0][0]:
-        if tailCommands[0][1] == 'D':
-            tailDirection += 1
-        else:
-            tailDirection -= 1
-        tailDirection %= 4
-        tailCommands.popleft()
+            direction = (direction + 1) % 4
+        index += 1
 
 print(time)
