829355daece9b7f30d918856a189829e394ad414
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-04-12 18:44:57 +0900

    [Refactor] DFS/BFS: 감시 피하기 새로운 방식으로 구현

--- 이코테/DFS:BFS/기출문제/감시 피하기.py
+++ 이코테/DFS:BFS/기출문제/감시 피하기.py
@@ -4,50 +4,53 @@ from itertools import combinations
 import copy
 moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-# 학생들이 모두 피해질 수 있는지 여부 함수
-def canAvoid(graph, teachers):
-    for i, j in teachers:
-        for move in moves:
-            x, y = i, j
-            # 한 방향에 대해 움직일 수 있을때까지 확인
-            while True:
-                nextX, nextY = x+move[0], y+move[1]
-                # 움직일 수 없는 경우 다음방향 탐색으로 이동
-                if nextX < 0 or nextX >= n:
-                    break
-                if nextY < 0 or nextY >= n:
-                    break
-                # 장애물인 경우 다음방향 탐색으로 이동
-                if graph[nextX][nextY] == 'O':
-                    break
-                # 학생을 만난 경우 종료
-                elif graph[nextX][nextY] == 'S':
-                    return False
-                x, y = nextX, nextY
-    return True
-
 n = int(input())
 graph = []
 empty = []
 teachers = []
-for i in range(n):
-    graph.append(list(input().split()))
-    for j in range(n):
-        if graph[i][j] == 'X':
-            empty.append((i, j))
-        elif graph[i][j] == 'T':
-            teachers.append((i, j))
+for x in range(n):
+    row = list(input().split())
+    graph.append(row)
+    for y in range(n):
+        if row[y] == 'X':
+            empty.append((x, y))
+        elif row[y] == 'T':
+            teachers.append((x, y))
+
+# 학생들이 모두 피해질 수 있는지 여부 함수
+def spread(graph, i, j):
+    for move in moves:
+        x, y = i, j
+        # 한 방향에 대해 움직일 수 있을때까지 확인
+        while True:
+            nextX, nextY = x+move[0], y+move[1]
+            # 움직일 수 없는 경우 다음방향 탐색으로 이동
+            if nextX < 0 or nextX >= n:
+                break
+            if nextY < 0 or nextY >= n:
+                break
+            # 장애물인 경우 다음방향 탐색으로 이동
+            if graph[nextX][nextY] == 'O':
+                break
+            # 학생을 만난 경우 종료
+            elif graph[nextX][nextY] == 'S':
+                return False
+            x, y = nextX, nextY
+    return True
 
 # 빈공간 중 3가지 조합
-can = False
+success = False
 for locations in combinations(empty, 3):
     checkGraph = copy.deepcopy(graph)
     for i, j in locations:
         checkGraph[i][j] = 'O'
     
     # 학생들 모두 피해지는 경우 종료
-    if canAvoid(checkGraph, teachers):
-        can = True
+    canAvoid = True
+    for i, j in teachers:
+        canAvoid &= spread(checkGraph, i, j)
+    if canAvoid:
+        success = True
         break
 
-print("YES" if can else "NO")
+print("YES" if success else "NO")
