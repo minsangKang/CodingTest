4f5dbd9ea164454d23fe743f1e3a4ad17a249248
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-11-24 16:35:38 +0900

    Programmers/L1 풀이 추가

--- Programmers/level1/Python/달리기 경주.py
+++ Programmers/level1/Python/달리기 경주.py
@@ -1,12 +1,12 @@
+# 추월한 선수의 이름: index (swap(index-1, index))
 def solution(players, callings):
-    # call -> index가 O(1)이 되어야 함
-    # 그리고 index -> name또한 가능해야 함
-    # dictionary로 call -> index를 하고
-    # index -> name은 배열로 관리
-    dict = { name: index for index, name in enumerate(players) }
-    for call in callings:
-        index = dict[call]
-        dict[call] -= 1
-        dict[players[index-1]] += 1
+    dict = { name: idx for idx, name in enumerate(players) }
+    for name in callings:
+        index = dict[name]
+        target = players[index-1]
+        
         players[index-1], players[index] = players[index], players[index-1]
+        dict[name] = index-1
+        dict[target] = index
+    
     return players
