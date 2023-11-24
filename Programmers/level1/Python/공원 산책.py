0f0eb3b3c19c8680dea682ccc3b69878269d1dc5
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-11-24 17:09:14 +0900

    Programmers/L1 풀이 추가

--- Programmers/level1/Python/공원 산책.py
+++ Programmers/level1/Python/공원 산책.py
@@ -1,28 +1,29 @@
 def solution(park, routes):
-    move = {'E':(0,1), 'W':(0,-1), 'N':(-1,0), 'S':(1,0)}
-    h, w = len(park), len(park[0])
+    dict = { 'N': (-1, 0), 'E': (0, 1), 'W': (0, -1), 'S': (1, 0) }
     y, x = 0, 0
-    # 시작위치 찾기
-    for i in range(h):
-        for j in range(w):
+    h, w = len(park), len(park[0])
+    # 시작지점 search
+    for i in range(len(park)):
+        for j in range(len(park[0])):
             if park[i][j] == 'S':
                 y, x = i, j
-    # 이동
-    for route in routes:
-        direct, length = route.split()[0], int(route.split()[1])
-        tempY, tempX = y, x
-        isBreak = False
-        
-        for i in range(length):
-            ny, nx = tempY+move[direct][0], tempX+move[direct][1]
-            if ny < 0 or ny >= h or nx < 0 or nx >= w:
-                isBreak = True
                 break
-            if park[ny][nx] == 'X':
-                isBreak = True
-                break
-            tempY, tempX = ny, nx
-        if isBreak == False:
-            y, x = tempY, tempX
-    return [y, x]
     
+    for route in routes:
+        print(y, x)
+        op, n = route.split()
+        ty, tx = y, x # 임시 y, x 저장
+        canGo = True
+        for i in range(1, int(n)+1):
+            ty, tx = y + dict[op][0]*i, x + dict[op][1]*i
+            # 벗어나는지, 장애물 확인
+            if ty < 0 or ty >= h or tx < 0 or tx >= w:
+                canGo = False
+                break
+            if park[ty][tx] == 'X':
+                canGo = False
+                break
+        # 이동
+        if canGo:
+            y, x = ty, tx
+    return [y, x]
