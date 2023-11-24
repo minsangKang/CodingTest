f000746c4573a0c2088f3bcca99a4462ce3f12aa
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-11-24 23:20:34 +0900

    Programmers/L1 풀이 추가

--- Programmers/level1/Python/카드 뭉치.py
+++ Programmers/level1/Python/카드 뭉치.py
@@ -1,10 +1,13 @@
 def solution(cards1, cards2, goal):
-    i, j = 0, 0
+    left_idx, right_idx = 0, 0
+    
     for target in goal:
-        if i < len(cards1) and cards1[i] == target:
-            i += 1
-        elif j < len(cards2) and cards2[j] == target:
-            j += 1
-        else:
-            return "No"
+        if left_idx < len(cards1) and target == cards1[left_idx]:
+            left_idx += 1
+            continue
+        if right_idx < len(cards2) and target == cards2[right_idx]:
+            right_idx += 1
+            continue
+        return "No"
+        
     return "Yes"
