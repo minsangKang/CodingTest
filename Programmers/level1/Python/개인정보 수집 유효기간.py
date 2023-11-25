42fa08521e53321c3892bfaa11f34153f2616f26
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-11-25 12:14:37 +0900

    Programmers/L1 풀이 추가

--- Programmers/level1/Python/개인정보 수집 유효기간.py
+++ Programmers/level1/Python/개인정보 수집 유효기간.py
@@ -1,18 +1,21 @@
 def solution(today, terms, privacies):
-    # 오늘 날짜로 파기해야 할 개인정보 번호들 반환
-    dict = { t.split()[0]: int(t.split()[1]) for t in terms }
-    ty, tm, td = map(int, today.split('.'))
+    dict = {}
+    for t in terms:
+        key, value = t.split()
+        dict[key] = int(value)
     
     result = []
-    for index, p in enumerate(privacies):
-        pday, pt = p.split()
-        py, pm, pd = map(int, pday.split('.'))
-        # 계약에 따라 날짜 계산
-        pm += dict[pt]
+    ty, tm, td = map(int, today.split("."))
+    today = ty*10000+tm*100+td
+    
+    for idx, privacy in enumerate(privacies):
+        pdate, case = privacy.split()
+        py, pm, pd = map(int, pdate.split("."))
+    
+        pm += dict[case]
         py += (pm-1)//12
         pm = pm%12 or 12
-        # today 와 py, pm, pd 비교
-        if ty*10000+tm*100+td >= py*10000+pm*100+pd:
-            result.append(index+1)
-    
+        
+        if py*10000+pm*100+pd <= today:
+            result.append(idx+1)
     return result
