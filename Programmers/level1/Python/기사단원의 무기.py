001fafb4586aff19537e00b0cdb03507cf8a3daf
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-11-29 19:12:18 +0900

    Programmers/L1 풀이 추가

--- Programmers/level1/Python/기사단원의 무기.py
+++ Programmers/level1/Python/기사단원의 무기.py
@@ -1,3 +1,4 @@
+# 약수구하기 (제곱근까지 확인, 약수로 나눈 값까지)
 def divisors(num):
     arr = [i for i in range(1, int(num**0.5)+1) if num%i == 0]
     arr += [num//i for i in arr]
@@ -6,8 +7,6 @@ def divisors(num):
 def solution(number, limit, power):
     result = []
     for i in range(1, number+1):
-        count = len(divisors(i))
-        result.append(count if count <= limit else power)
-    
+        divisorsCount = len(divisors(i))
+        result.append(divisorsCount if divisorsCount <= limit else power)
     return sum(result)
-    
