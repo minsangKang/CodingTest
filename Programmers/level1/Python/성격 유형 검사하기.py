7b054dfbc38e1e0efd0e640d67ebcf3b4b520faa
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-12-12 11:30:57 +0900

    Programmers/L1 풀이 추가

--- Programmers/level1/Python/성격 유형 검사하기.py
+++ Programmers/level1/Python/성격 유형 검사하기.py
@@ -1,21 +1,13 @@
-def type(dict, leftKey, rightKey):
-    return leftKey if dict[leftKey] == max(dict[leftKey], dict[rightKey]) else rightKey
-
 def solution(survey, choices):
-    dict = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
+    dict = {'RT':0, 'CF':0, 'JM':0, 'AN':0}
+    for answer, choice in zip(survey, choices):
+        if answer not in dict:
+            answer = answer[::-1]
+            dict[answer] += 4-choice
+        else:
+            dict[answer] += choice-4
     
-    for i in range(len(survey)):
-        score = choices[i] - 4
-        if score < 0:
-            dict[survey[i][0]] += (-score)
-        elif score > 0:
-            dict[survey[i][1]] += score
-    type1 = type(dict, "R", "T")
-    type2 = type(dict, "C", "F")
-    type3 = type(dict, "J", "M")
-    type4 = type(dict, "A", "N")
-    answer = type1+type2+type3+type4
-    return answer
-
-answer = solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5])
-print(answer)
+    result = ""
+    for key in dict.keys():
+        result += key[1] if dict[key] > 0 else key[0]
+    return result
